41d2f4302bb88f511c2fba9ce20ee7a5ebb59286
ismaelpaul <ismaafp@gmail.com>
2023-07-16 20:12:49 +0100

    Add isSidebarOpen conditional and Nav component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import "../app.css";
+	import Nav from "../lib/nav.svelte";
+	import Sidebar from "../lib/sidebar.svelte";
+
+    let isSidebarOpen = false;
+
+
+    export const toggleSidebar = () => {
+    isSidebarOpen = !isSidebarOpen
+}
+</script>
+<div class="relative">
+
+    {#if isSidebarOpen}
+    <div class="flex flex-row-reverse w-[100vw] h-[100vh]">
+        
+            <Sidebar on:toggle={toggleSidebar} />
+            <div class="bg-dark-space-blue opacity-50 w-full h-full"></div>
+        </div>
+    {/if}
+    <Nav on:toggle={toggleSidebar}/>
+</div>
+
+<style lang="postcss">
+
+</style>
