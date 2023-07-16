6919bb9f55b0e57597ac09b89190292b06c5f968
ismaelpaul <ismaafp@gmail.com>
2023-07-16 20:13:43 +0100

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		colors: {
+			'almost-white': '#FFFDFA',
+			'dark-space-blue': '#00001A',
+			gunmetal: '#5E607A',
+			'light-vermillion': '#F15D51',
+			silver: 'C5C6CE',
+			yellow: '#E9AA52',
+		},
 		extend: {},
 	},
 	plugins: [],
