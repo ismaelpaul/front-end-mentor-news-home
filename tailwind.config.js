e8c7570d82bd9e5e9310cd964bb1d08a4043840f
ismaelpaul <ismaafp@gmail.com>
2023-08-26 11:13:51 +0100

    Change color and add fontSize

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,38 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		colors: {
-			'almost-white': '#FFFDFA',
-			'dark-space-blue': '#00001A',
+			'off-white': '#FFFDFA',
+			'very-dark-blue': '#00001A',
+			'dark-grayish-blue': '#5D5F79',
+			'grayish-blue': '#C5C6CE',
 			gunmetal: '#5E607A',
 			'light-vermillion': '#F15D51',
 			silver: 'C5C6CE',
 			yellow: '#E9AA52',
+			'soft-orange': '#FFCE8A',
+		},
+		fontSize: {
+			header: [
+				'2.6rem',
+				{
+					lineHeight: '2.5rem',
+					fontWeight: '800',
+				},
+			],
+			paragraph: [
+				'1rem',
+				{
+					lineHeight: '1.625rem',
+					fontWeight: '300',
+				},
+			],
+			button: [
+				'0.875rem',
+				{
+					lineHeight: '2rem',
+					fontWeight: '600',
+				},
+			],
 		},
 		extend: {},
 	},
