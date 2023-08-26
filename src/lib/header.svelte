d922ead18f84ae7a70d6301dd622bd231aef809d
ismaelpaul <ismaafp@gmail.com>
2023-08-26 11:11:37 +0100

    Add header component

---
+++ src/lib/header.svelte
@@ -0,0 +1,8 @@
+<header class="mx-4 flex flex-col gap-8">
+    <img src="images/image-web-3-mobile.jpg" alt="header">
+    <div class="flex flex-col gap-4">
+        <h1 class="text-header text-very-dark-blue">The Bright Future of Web 3.0?</h1>
+        <p class="text-paragraph text-gunmetal">We dive into the next evolution of the web that claims to put the power of the platform back into the hands of the people. But is it really fulfilling its promise?</p>
+    </div>
+    <button class="text-off-white text-button bg-light-vermillion px-[1.938rem] py-2 tracking-[0.274rem] w-[11.563rem]">READ MORE</button>
+</header>
