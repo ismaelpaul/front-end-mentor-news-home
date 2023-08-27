cdf9c0f251f7ad2166eba4dda3f04db441468e8a
ismaelpaul <ismaafp@gmail.com>
2023-08-27 19:11:47 +0100

    Add laptop media query

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,8 +1,11 @@
-<header class="mx-4 flex flex-col gap-8 mb-16">
-    <img src="images/image-web-3-mobile.jpg" alt="header">
-    <div class="flex flex-col gap-4">
-        <h1 class="text-header text-very-dark-blue">The Bright Future of Web 3.0?</h1>
-        <p class="text-paragraph text-gunmetal">We dive into the next evolution of the web that claims to put the power of the platform back into the hands of the people. But is it really fulfilling its promise?</p>
+<header class="mx-4 flex flex-col gap-8 mb-16 laptop:mb-0 laptop:mx-0  laptop:col-span-2 laptop:row-span-2">
+    <img class="laptop:hidden" src="images/image-web-3-mobile.jpg" alt="header">
+    <img class="hidden laptop:inline-block laptop:h-[18.75rem]" src="images/image-web-3-desktop.jpg" alt="header">
+    <div class="flex flex-col gap-4 laptop:flex-row laptop:gap-[1.875rem]">
+        <h1 class="text-header text-very-dark-blue laptop:text-x-large-heading laptop:basis-full">The Bright Future of Web 3.0?</h1>
+        <div class="laptop:flex laptop:flex-col laptop:basis-full laptop:gap-[1.813rem]">
+            <p class="text-paragraph text-gunmetal laptop:basis-full">We dive into the next evolution of the web that claims to put the power of the platform back into the hands of the people. But is it really fulfilling its promise?</p>
+            <button class="text-off-white text-button bg-light-vermillion px-[1.938rem] py-2 tracking-[0.274rem] w-[11.563rem] hover:bg-very-dark-blue">READ MORE</button>
+        </div>
     </div>
-    <button class="text-off-white text-button bg-light-vermillion px-[1.938rem] py-2 tracking-[0.274rem] w-[11.563rem]">READ MORE</button>
 </header>
