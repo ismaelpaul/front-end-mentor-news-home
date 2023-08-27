6af19cad8eeb89d5115040a3755ee7dea9d4961a
ismaelpaul <ismaafp@gmail.com>
2023-08-27 19:14:22 +0100

    Add container and subHeading variables that contains utility classes

--- src/lib/new.svelte
+++ src/lib/new.svelte
@@ -1,15 +1,19 @@
-<div class="bg-very-dark-blue mx-4 px-[1.313rem] py-6">
+<script>
+    const containerClass = 'border-gunmetal border-b mb-[1.938rem]'
+    const subHeadingClass = 'text-small-heading text-off-white cursor-pointer mb-2 hover:text-yellow'
+</script>
+<div class="bg-very-dark-blue mx-4 px-[1.313rem] py-6 mb-16 laptop:mb-0 laptop:mx-0">
     <h1 class="text-medium-heading text-yellow mb-8">New</h1>
-    <div class=" border-gunmetal border-b mb-[1.938rem]">
-        <h2 class="text-small-heading text-off-white mb-2">Hydrogen vs Electric Cars</h2>
+    <div class={containerClass}>
+        <h2 class={subHeadingClass}>Hydrogen vs Electric Cars</h2>
         <p class="text-paragraph text-grayish-blue mb-[1.813rem]">Will hydrogen-fueled cars ever catch up to EVs?</p>
     </div>
-    <div class=" border-gunmetal border-b mb-[1.938rem]">
-        <h2 class="text-small-heading text-off-white mb-2">The Downsides of AI Artistry</h2>
+    <div class={containerClass}>
+        <h2 class={subHeadingClass}>The Downsides of AI Artistry</h2>
         <p class="text-paragraph text-grayish-blue mb-[1.813rem]">What are the possible adverse effects of on-demand AI image generation?</p>
     </div>
    <div>
-       <h2 class="text-small-heading text-off-white mb-2">Is VC Funding Drying Up?</h2>
+       <h2 class={subHeadingClass}>Is VC Funding Drying Up?</h2>
        <p class="text-paragraph text-grayish-blue">Private funding by VC firms is down 50% YOY. We take a look at what that means.</p>
    </div>
    
