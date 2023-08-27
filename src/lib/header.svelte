41389fe399166da9e8287384c8f87636c3163103
ismaelpaul <ismaafp@gmail.com>
2023-08-27 19:20:50 +0100

    Minor changes on flexbox

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -3,7 +3,7 @@
     <img class="hidden laptop:inline-block laptop:h-[18.75rem]" src="images/image-web-3-desktop.jpg" alt="header">
     <div class="flex flex-col gap-4 laptop:flex-row laptop:gap-[1.875rem]">
         <h1 class="text-header text-very-dark-blue laptop:text-x-large-heading laptop:basis-full">The Bright Future of Web 3.0?</h1>
-        <div class="laptop:flex laptop:flex-col laptop:basis-full laptop:gap-[1.813rem]">
+        <div class="flex flex-col gap-6 laptop:basis-full laptop:gap-[1.813rem]">
             <p class="text-paragraph text-gunmetal laptop:basis-full">We dive into the next evolution of the web that claims to put the power of the platform back into the hands of the people. But is it really fulfilling its promise?</p>
             <button class="text-off-white text-button bg-light-vermillion px-[1.938rem] py-2 tracking-[0.274rem] w-[11.563rem] hover:bg-very-dark-blue">READ MORE</button>
         </div>
