fab843867f622700cfc95650ef45b961c637ad4b
ismaelpaul <ismaafp@gmail.com>
2023-08-27 12:59:44 +0100

    Add card reusable component

---
+++ src/lib/card.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let element
+</script>
+
+<article class="flex flex-row gap-6">
+    <img class="max-w-[33%] h-32" src={element.image} alt='retro pc'/>
+    <div>
+        <h2 class="text-medium-heading text-grayish-blue mb-3">0{element.id}</h2>
+        <h3 class="text-x-small-heading mb-[0.563rem]">{element.title}</h3>
+        <p class="text-paragraph text-gunmetal">{element.text}</p>
+    </div>
+</article>
