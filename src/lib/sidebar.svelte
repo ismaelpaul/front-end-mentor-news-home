739aa46c571d9cc54a730f66d44c7c59164b7e50
ismaelpaul <ismaafp@gmail.com>
2023-08-27 19:15:25 +0100

    Minor adjustments on styling

--- src/lib/sidebar.svelte
+++ src/lib/sidebar.svelte
@@ -7,10 +7,10 @@
   }
 </script>
 
-    <div class="bg-almost-white h-screen absolute z-10 w-[50vw] px-4 py-7">
-        <input type="image" class="w-[1.875rem] ml-auto" src="/images/icon-menu-close.svg" alt="close menu" on:click={toggle}>
-        <nav class="mt-[5.375rem]">
-            <ul class="flex flex-col gap-6">
+    <div class="flex flex-col items-end bg-off-white h-screen absolute z-10 w-[70vw] px-4 py-7">
+        <input type="image" class="w-[1.875rem]" src="/images/icon-menu-close.svg" alt="close menu" on:click={toggle}>
+        <nav class="mt-[5.375rem] mr-auto">
+            <ul class="text-sidebar-nav flex flex-col gap-6">
                 <li>Home</li>
                 <li>New</li>
                 <li>Popular</li>
